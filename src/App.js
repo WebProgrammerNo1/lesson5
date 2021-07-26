d51eae9bf594b42a9bbe43f7f4822f9bd8c418de
Jaloliddin <arabshox007@gmail.com>
2021-07-26 11:27:53 +0500

    commentariya

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					<code>
+						src/Appsadasdkjasjdjashk dsadasda
+						dasfbasfnsamfnsamdnm,sdnasadasdassan,n,mnfsmdnsafmasdbfajwekja.fn.dsmfnmanfmnsd,.fmna,.sdmnf,.asmdnf.,amnsd.fnmasf,anfmanfmna,smnfmans,fmnasmfn,samnfmasn.js
+					</code>{" "}
+					and save to reload.
+				</p>
+				<div>Hello world</div>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
